473c3a54c0353b0f09ae9ec6b7c651611afc8bd4
Nick Hirakawa <nh0815@gmail.com>
2015-08-24 21:41:30 -0400

    Add accessor methods for start and end cells

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import maze.Maze;
+import viewer.MazeViewer;
+
 /**
  * Created by nick on 8/23/15.
  */
 public class Main {
 
     public static void main(String[] args){
-        
+        Maze maze = new Maze(20, 20);
+        maze.generate();
+        MazeViewer viewer = new MazeViewer(maze, 20);
     }
 }
